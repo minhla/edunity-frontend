6b552b9388b3a94310954272d3b1e6647a4af39e
minh-le-jh <anthonyle9x@gmail.com>
2024-11-03 18:09:55 +0700

    Update footer to be responsive

--- components/custom/footer.tsx
+++ components/custom/footer.tsx
@@ -4,14 +4,16 @@ const Footer = () => {
   return (
     <footer className="bg-edunity-black text-white">
       <div className="content-wrapper">
-        <div className="flex items-center justify-center min-h-[80px] gap-1">
-        <span> Copyright © 2024 </span>
-        <Link href="/" className="text-edunity-primary font-body font-bold">
-          <span>edunity</span>
-        </Link>
-        <span> || All Rights Reserved</span>
+        <div className="flex flex-col md:flex-row items-center justify-center min-h-[80px] gap-2">
+          <div className="md:after:content-['||'] md:after:relative md:after:ml-2">
+            <span> Copyright © 2024 </span>
+            <Link href="/" className="text-edunity-primary font-body font-bold">
+              <span>edunity</span>
+            </Link>
+          </div>
+
+          <span> All Rights Reserved</span>
         </div>
-       
       </div>
     </footer>
   );
